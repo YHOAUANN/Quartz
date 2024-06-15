e499f0f93c6d555088a867666d294b500d7753cc
yhoauann <yhoauann@qq.com>
2024-06-15 13:01:32 +0800

    Quartz sync: Jun 15, 2024, 1:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: " 伊霍安的数字花园🌱",
+    pageTitle: " 伊霍安的Note🌱",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
